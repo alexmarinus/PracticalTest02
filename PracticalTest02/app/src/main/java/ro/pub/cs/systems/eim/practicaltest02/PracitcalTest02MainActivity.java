c5348e353ff446f84d8bc015bfa36de7224c9ff9
Student User <student@cti.pub.ro>
2017-05-23 09:41:22 +0300

    tasks for col 2

---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracitcalTest02MainActivity.java
@@ -0,0 +1,13 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PracitcalTest02MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pracitcal_test02_main);
+    }
+}
